6d92d95251ba8cbbe6afb4b8b3a8667735a1ba90
duffisberg <duffisberg@gmail.com>
2023-10-21 17:43:14 -0400

    Basic Navbar finished, created App routes and initial Page structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignUp from "./Pages/LoginSignUp";
+import ShopCategory from "./Pages/ShopCategory";
+import Home from "./Pages/Home";
+import About from "./Pages/About"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/shop' element = {<Shop />} />
+        <Route path='/mens' element = {<ShopCategory category = "mens" />} />
+        <Route path='/womens' element = {<ShopCategory category = "womens" />} />
+        <Route path='/login' element = {<LoginSignUp />} />
+        <Route path='/product' element = {<Product />}>
+          <Route path = ":productId" element = {<Product />}/>
+        </Route>
+        <Route path='/cart' element = {<Cart />} /> 
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,25 @@
-import React from "react";
+import {React} from "react";
 import "./Navbar.css";
 import logo from "../Assets/AF_Logo_W.png";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { IconContext } from "react-icons";
+
+
 
 export const Navbar = () => {
+
   return (
     <div className="navbar">
-      <ul className="nav-menu">
-        <li>Home</li>
-        <li>Shop</li>
-      </ul>
       <div className="nav-logo">
         <img src={logo} alt="Antique Fists Logo" id="nav-logo"></img>
       </div>
       <ul className="nav-menu">
         <li>About</li>
         <li>Videos</li>
+        <li>Home</li>
+        <li>Shop</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
-        <IconContext.Provider
-          value={{ color: "white", className: "shopping-cart-icon" }}
-        >
-          <AiOutlineShoppingCart value={{ color: "white" }} />
-        </IconContext.Provider>
+        <div className = "cart">Cart<span className = "bracket">(</span>0<span className = "bracket">)</span></div>
       </div>
     </div>
   );
