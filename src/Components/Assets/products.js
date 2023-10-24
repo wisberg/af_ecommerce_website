8fa886cd221cce24be8ad6c3c1ae5c71034defd6
duffisberg <duffisberg@gmail.com>
2023-10-23 20:15:28 -0400

    Continuing to work on Item level UI in the store area.

--- src/Components/Assets/products.js
+++ src/Components/Assets/products.js
@@ -20,6 +20,51 @@ let all_products = [
     description:
       "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
   },
+  {
+    id: 2,
+    name: "Antique Fists White T-Shirt",
+    category: "men",
+    image: AF_Shirt_Black,
+    price: 30,
+    description:
+      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+  },
+  {
+    id: 2,
+    name: "Antique Fists White T-Shirt",
+    category: "men",
+    image: AF_Shirt_Black,
+    price: 30,
+    description:
+      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+  },
+  {
+    id: 2,
+    name: "Antique Fists White T-Shirt",
+    category: "men",
+    image: AF_Shirt_Black,
+    price: 30,
+    description:
+      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+  },
+  {
+    id: 2,
+    name: "Antique Fists White T-Shirt",
+    category: "men",
+    image: AF_Shirt_Black,
+    price: 30,
+    description:
+      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+  },
+  {
+    id: 2,
+    name: "Antique Fists White T-Shirt",
+    category: "men",
+    image: AF_Shirt_Black,
+    price: 30,
+    description:
+      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+  },
 ];
 
 export default all_products;
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./item.css";
+import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
 
 const Item = (props) => {
   return (
     <div className="item">
       <img className="item-image" src={props.image} alt="" />
-      <p className="item-name">{props.name}</p>
-      <div className="item-price">${props.price}</div>
-      <div className="item-buttons">
-        <button className="add-to-cart">Add to Cart</button>
-        <button className="view-item">View More</button>
+      <div className="item-info">
+        <p className="item-name">{props.name}</p>
+        <div className="item-price">${props.price}</div>
       </div>
     </div>
   );
 };
 
 export default Item;
+
+{
+  /* <div className="item-buttons">
+  <button className="add-to-cart">
+    Add to Cart <AiOutlineShoppingCart />
+  </button>
+  <button className="view-item">
+    View More <AiOutlineSearch />
+  </button>
+</div>; */
+}
