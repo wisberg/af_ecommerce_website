8c19dc7613ca5e3673431e7ec5d8128095e704dd
duffisberg <duffisberg@gmail.com>
2023-10-26 13:01:44 -0400

    Updated product database

--- src/Components/Assets/products.js
+++ src/Components/Assets/products.js
@@ -1,42 +1,72 @@
-import AF_Shirt_White from "./Product-Images/AF_Shirt_W_Front.png";
-import AF_Shirt_Black from "./Product-Images/AF_Shirt_W_Back.png";
+import AF_Shirt_White_Front from "./Product-Images/AF_Shirt_W_Front.png";
+import AF_Shirt_White_Back from "./Product-Images/AF_Shirt_W_Back.png";
+import AF_Hat_B from "./Product-Images/AF_Hat_B.png";
+import AF_Hat_W from "./Product-Images/AF_Hat_W.png";
+import AF_Hoodie_B_Back from "./Product-Images/AF_Hoodie_B_Back.png";
+import AF_Hoodie_B_Front from "./Product-Images/AF_Hoodie_B_Front.png";
+import AF_Hoodie_W_Back from "./Product-Images/AF_Hoodie_W_Back.png";
+import AF_Hoodie_W_Front from "./Product-Images/AF_Hoodie_W_Front.png";
+import AF_Shirt_B_Back from "./Product-Images/AF_Shirt_B_Back.png";
+import AF_Shirt_B_Front from "./Product-Images/AF_Shirt_B_Front.png";
 
 let all_products = [
   {
     id: 1,
+    name: "Antique Fists White T-Shirt",
+    category: "men",
+    image: AF_Shirt_White_Front,
+    image_back: AF_Shirt_White_Back,
+    price: 30,
+    description:
+      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+  },
+  {
+    id: 2,
     name: "Antique Fists Black T-Shirt",
     category: "men",
-    image: AF_Shirt_White,
+    image: AF_Shirt_B_Front,
+    image_back: AF_Shirt_B_Back,
     price: 30,
     description:
       "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
   },
   {
-    id: 2,
-    name: "Antique Fists White T-Shirt",
+    id: 3,
+    name: "Antique Fists White Hoodie",
     category: "men",
-    image: AF_Shirt_Black,
-    price: 30,
+    image: AF_Hoodie_W_Front,
+    image_back: AF_Hoodie_W_Back,
+    price: 60,
     description:
-      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+      "Experience the unparalleled comfort and lasting quality of Antique Fists' White Hoodie, expertly crafted with premium materials.",
   },
   {
-    id: 2,
-    name: "Antique Fists White T-Shirt",
+    id: 4,
+    name: "Antique Fists Black Hoodie",
     category: "men",
-    image: AF_Shirt_Black,
-    price: 30,
+    image: AF_Hoodie_B_Front,
+    image_back: AF_Hoodie_B_Back,
+    price: 60,
     description:
-      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+      "Experience the unparalleled comfort and lasting quality of Antique Fists' Black Hoodie, expertly crafted with premium materials.",
   },
   {
-    id: 2,
-    name: "Antique Fists White T-Shirt",
+    id: 5,
+    name: "Antique Fists Black Cap",
     category: "men",
-    image: AF_Shirt_Black,
-    price: 30,
+    image: AF_Hat_B,
+    price: 25,
     description:
-      "Perfect blend of style and comfort. This must-have t-shirt features our iconic logo on both the front and back, making a bold statement wherever you go. Crafted with premium materials, it's the ultimate choice for your everyday wardrobe.",
+      "Embrace the comfort and style of our Dad Hat, featuring our logo on the front, expertly crafted with premium materials.",
+  },
+  {
+    id: 6,
+    name: "Antique Fists White Cap",
+    category: "men",
+    image: AF_Hat_W,
+    price: 25,
+    description:
+      "Embrace the comfort and style of our Dad Hat, featuring our logo on the front, expertly crafted with premium materials.",
   },
 ];
 
